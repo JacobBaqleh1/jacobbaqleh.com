c586fcd145bd20198194df5bd6deb17694dfc731
Jacob Baqleh <101436252+JacobBaqleh1@users.noreply.github.com>
2023-03-12 19:06:34 -0700

    update some UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default function Navbar() {
             Skills
           </a>
           <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
+            About Me
           </a>
         </nav>
         <a
--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
-export const projects = [{
+export const projects = [
+  {
  
     link: 'https://www.google.com',
     image: 'https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y29mZmVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
@@ -6,12 +7,47 @@ export const projects = [{
     subtitle: 'Javascript and Css',
     description: 'BUilt'
   } ,
-  {title: "DevChat",
+  {
+    title: "DevChat",
   subtitle: "React and Firebase",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
   image: "https://images.unsplash.com/photo-1625805866449-3589fe3f71a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZGVvJTIwZ2FtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-  link: "https://jsbootcamp.com",}
+  link: "https://jsbootcamp.com",
+} ,
+{
+  title: "DevChat",
+subtitle: "React and Firebase",
+description:
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+image: "https://images.unsplash.com/photo-1625805866449-3589fe3f71a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZGVvJTIwZ2FtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+link: "https://jsbootcamp.com",
+} ,
+{
+  title: "DevChat",
+subtitle: "React and Firebase",
+description:
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+image: "https://images.unsplash.com/photo-1625805866449-3589fe3f71a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZGVvJTIwZ2FtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+link: "https://jsbootcamp.com",
+} ,
+{
+  title: "DevChat",
+subtitle: "React and Firebase",
+description:
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+image: "https://images.unsplash.com/photo-1625805866449-3589fe3f71a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZGVvJTIwZ2FtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+link: "https://jsbootcamp.com",
+} ,
+{
+  title: "DevChat",
+subtitle: "React and Firebase",
+description:
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+image: "https://images.unsplash.com/photo-1625805866449-3589fe3f71a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZGVvJTIwZ2FtZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+link: "https://jsbootcamp.com",
+} ,
+
   ]
   
-  export const skills = ['javascropt', 'node']
+  export const skills = ['HTML', 'CSS', 'Javascript', 'Tailwind', 'Git', 'Node']
